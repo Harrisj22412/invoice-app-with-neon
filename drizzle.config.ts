a93f84847133e262e0d7cd038183fa804b4d7b34
Jalen Harris <Harrisj22412@hocking.edu>
2024-11-18 07:34:29 -0500

    - added use client - Using POST request method to update the user's bank info and GET request to get bank info from db using the user's ID -updated path for connection to db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ if (!process.env.NEON_DATABASE_URL)
     throw new Error("NEON DATABASE_URL not found in environment");
 
 export default {
-    schema: "./src/app/db/schema.ts",
+    schema: "./app/db/schema.ts",
     out: "./src/app/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
